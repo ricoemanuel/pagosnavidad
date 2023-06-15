25f9cd2989bde57d435e568c3c3008b6b457edbf
unknown <erico@enovel.com.co>
2023-06-14 20:38:07 -0500

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'innovationhubes',
+    appId: '1:516056036769:web:75ef7c6b14947d5f9307e0',
+    storageBucket: 'innovationhubes.appspot.com',
+    apiKey: 'AIzaSyCEAuwGVK6oNRJxmBI4LXJbU98a7fKAACg',
+    authDomain: 'innovationhubes.firebaseapp.com',
+    messagingSenderId: '516056036769',
+    measurementId: 'G-CYGDC6QGV1',
+  },
+};
