f0306ed3c6eb387cca2dbc8c0ff83c4a09b72d9e
unknown <erico@enovel.com.co>
2023-07-03 21:13:27 -0500

    reglas de seguridad

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 import { FirebaseService } from './services/firebase.service';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,16 +10,16 @@ export class AppComponent implements OnInit {
   title = 'innovationhubes';
   user: string | null = null
   load: boolean = false
-  constructor(private firebase: FirebaseService, private route: Router) {
-    localStorage.setItem("registrando","false")
+  tamanoPantalla!: number
+  developingMode: boolean = true
+  constructor(private firebase: FirebaseService, private route: Router, private elementRef: ElementRef) {
+    localStorage.setItem("registrando", "false")
   }
   async ngOnInit(): Promise<void> {
-
     await this.getUser()
     setTimeout(() => {
       this.load = true; // Ocultar el indicador de carga
     }, 1500);
-
   }
   async getUser() {
     this.firebase.getAuthState().subscribe(res => {
@@ -33,5 +32,20 @@ export class AppComponent implements OnInit {
         }
       }
     })
+  }
+  @HostListener('contextmenu', ['$event'])
+  onContextMenu(event: MouseEvent) {
+    if (!this.developingMode) {
+      event.preventDefault();
+    }
+  }
+  @HostListener('document:mouseleave', ['$event'])
+  onDocumentMouseLeave(event: MouseEvent) {
+    const isMouseOutside = !this.elementRef.nativeElement.contains(event.target as Node);
+    if (isMouseOutside) {
+      if(!this.developingMode){
+        window.location.href="https://www.google.com/"
+      }
+    }
   }
 }
